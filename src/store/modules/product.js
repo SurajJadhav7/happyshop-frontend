813054f43744044fd655a3044f491b5732552265
Suraj Jadhav <jadhavsuraj777@gmail.com>
2021-10-09 00:38:02 +0530

    updated backend url from localhost to happyshop-backend

--- src/store/modules/product.js
+++ src/store/modules/product.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-// const baseURL = "https://happyshop-backend.herokuapp.com/";
-const baseURL = "http://localhost:3000/";
+const baseURL = "https://happyshop-backend.herokuapp.com/";
+// const baseURL = "http://localhost:3000/";
 
 const state = {
     products: [],
